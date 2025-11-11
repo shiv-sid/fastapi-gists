2d1a7772506b2b920d064f07f346424b455c3756
nshivsid <shivasiddharthslm@gmail.com>
2025-11-11 11:05:20 +0530

    setup.py updated

--- setup.py
+++ setup.py
@@ -4,11 +4,5 @@ setup(
     name="fastapi_gists_app",
     version="0.1.0",
     packages=find_packages(),
-    install_requires=[
-        "fastapi",
-        "uvicorn",
-        "requests",
-        "pytest",
-        "httpx"
-    ],
+    install_requires=["fastapi", "uvicorn", "requests", "pytest", "httpx"],
 )
